29f26dc54bb03979982ea702839bd9240634f941
Johnson, Jacob <jacob.johnson@fmr.com>
2017-08-14 12:30:42 -0400

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *    File Name: HelloWorld.java
+ *
+ *    Author: a583410
+ *
+ *    Copyright 2017 FMR LLC.
+ *
+ *    This software and all information contained herein is the property
+ *    of Fidelity Investments.  Much of this information including ideas,
+ *    concepts, formulas, processes, data, know-how, techniques, and
+ *    the like, found herein is considered proprietary to Fidelity
+ *    Investments, and may be covered by U.S. and foreign patents or
+ *    patents pending, or protected under trade secret laws.
+ *    Any dissemination, disclosure, use, or reproduction of this
+ *    material for any reason inconsistent with the express purpose
+ *    for which it has been disclosed is strictly forbidden.
+ *
+ *                        Restricted Rights Legend
+ *                        ------------------------
+ *
+ *    Use, duplication, or disclosure by the Government is subject to
+ *    restrictions as set forth in paragraph (b)(3)(B) of the Rights in
+ *    Technical Data and Computer Software clause in DAR 7-104.9(a).
+ ******************************************************************************/
+
+/**
+ * @author a583410
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		// Prints "Hello, World" in the terminal window.
+		System.out.println("Hello, World");
+	}
+}
